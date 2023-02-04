1ebc20b387793480b186f2723c2256bda6e06050
ihwaseok <39079669+ihwaseok@users.noreply.github.com>
2023-02-04 16:57:14 +0900

    주석 추가

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 외부 모듈 가져오기
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,18 +8,21 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// express 애플리케이션 생성
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 미들웨어 함수나 함수를 사용 가능한 상태로 준비한다 (마운트)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 페이지 연결
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -38,4 +42,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// 외부에서 require를 위해 지정
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); // 라우터 객체 생성
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
