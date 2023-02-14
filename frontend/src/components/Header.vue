c94dd7f19c7b5725e0f2745df69c112bb47d67d2
ihwaseok <39079669+ihwaseok@users.noreply.github.com>
2023-02-14 15:10:36 +0900

    헤더 형태 완성
    헤더 형태 완성
    기본 페이지 설정

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="sticky-top header-bg-white">
-    <header class="d-flex flex-wrap justify-content-center py-3">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Simple header</span>
+    <header class="d-flex flex-wrap justify-content-center py-1">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none header-logo">
+        <span class="fs-4">LSH Portpolio Site</span>
         </a>
 
-        <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <ul class="nav nav-pills header-nav">
+            <li class="nav-item" v-for="(item) of menuList">
+                <a href="#" class="nav-link" v-bind:id="item.id" v-bind:class="{active: (selectedId == item.id)}" v-on:click="menuSelect(item.id)">
+                    {{ item.name }}
+                </a>
+            </li>
+            <li class="nav-item"><a href="#" class="nav-link"><font-awesome-icon icon="phone"/></a></li>
         </ul>
     </header>
 </div>
@@ -19,11 +19,27 @@
 
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+
+const menuList = [{id: '1', name: '공부 기록'}, {id: '2', name: '테스트'}]
+let selectedId: Ref<string> = ref('1');
+
+function menuSelect (menuId: string): void {
+    selectedId.value = menuId;
+}
 </script>
 
 
 <style scoped>
 .header-bg-white {
     background-color: #9bc3fe;
+}
+.header-logo {
+    padding-left: 0.65rem;
+}
+.header-nav {
+    margin-right: 4rem;
 }
 </style>
--- frontend/src/components/JoplinPage.vue
+++ frontend/src/components/JoplinPage.vue
@@ -29,6 +29,10 @@ function getHtmlText (path: string) {
         });
 }
 
+// Init
+const index: string = 'C:\\Users\\Seok\\Documents\\GitHub\\PortpolioSite\\backend\\dist\\public\\joplin\\index.html';
+getHtmlText(index);
+
 // App 에서 getHtmlText를 호출할 수 있도록 설정
 defineExpose({ getHtmlText });
 
