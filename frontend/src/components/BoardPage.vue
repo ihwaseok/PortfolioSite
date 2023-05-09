97b1a60301a820149211f2a2f38d05552bd6215a
ihwaseok <39079669+ihwaseok@users.noreply.github.com>
2023-05-09 20:48:48 +0900

    검색기능 추가

--- frontend/src/components/BoardPage.vue
+++ frontend/src/components/BoardPage.vue
@@ -9,7 +9,11 @@
             <div>
                 <RouterLink role="button" id="btnWrite" to="/board/write" class="btn btn-primary" style="margin-bottom: 1rem;">글쓰기</RouterLink>
             </div>
-            
+            <div class="input-group mb-3" style="width: 20%;">
+                <input type="text" id="searchText" v-model="searchText" class="form-control" placeholder="제목 검색">
+                <button id="btnSearch" v-bind:onClick="search" class="btn btn-outline-secondary" type="button">검색</button>
+            </div>
+
             <Table v-bind:header="indexHeader" v-bind:headerMatcher="indexHeaderMatcher" v-bind:dataList="indexDataList" v-bind:rowFunction="goDetailPage" />
         </div>
         
@@ -30,6 +34,7 @@ import Router from '@/router';
 const indexHeader: string[] = ['분류', 'BOARD_ID', '제목', '내용', 'CREATED_BY', 'CREATED_AT', 'DELETE_YN'];
 const indexHeaderMatcher: string[] = ['CATEGORY', 'BOARD_ID', 'TITLE', 'CONTENT', 'CREATED_BY', 'CREATED_AT', 'DELETE_YN'];
 let indexDataList: Ref<ADMIN_BOARD[]> = ref([]);
+let searchText: string = "";
 
 // 상세 페이지 이동
 const goDetailPage: Function = function(line: string[]) {
@@ -47,6 +52,19 @@ function getBoardList (path: string): void {
             console.log(error);
             alert('에러 발생');
         });       
+}
+
+// 검색
+function search (): void {
+    Axios.get('/board/boardList/search', {params: {searchText: searchText}})
+        .then((res: AxiosResponse) => {                
+            const dataList: ADMIN_BOARD[] = res.data;
+            indexDataList.value = dataList;
+        })
+        .catch((error: AxiosError) => {
+            console.log(error);
+            alert('에러 발생');
+        });
 }
 
 getBoardList('all');
